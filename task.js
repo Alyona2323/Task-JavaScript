37be348a451c0f4aae5b38a5169e07e0a64607fc
alenasidoruk09@gmail.com <alenasidoruk09@gmail.com>
2022-09-13 20:58:38 +0300

    my first commit js

---
+++ task.js
@@ -0,0 +1,5 @@
+const input = prompt("What is Bitcoin price today?");
+alert(`Bitcoin price today ${input}.`);
+const question = prompt("How much $ do you have?");
+alert(`I have ${question}$.`); 
+alert(`You can buy ${question/input} BTC.`);
